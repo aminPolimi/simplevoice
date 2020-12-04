f345399887ea6350c4b046d0200a57f9ebaa1c1a
Amin Fathi <amin.fathi65@gmail.com>
2020-12-04 16:12:52 +0100

    Update GetCommandIntentHandler.swift
    add comment

--- ios/CommandIntent/GetCommandIntentHandler.swift
+++ ios/CommandIntent/GetCommandIntentHandler.swift
@@ -15,6 +15,12 @@ class GetCommandInentHandler: NSObject, GetCommandIntentHandling{
     let req = intent.request!
     let res = "the result is " + req
     
+    
+    
+    
+    // I need to send {req} to react-native
+    // then receive a simple text message from react-native
+    // finally set {result} in the next line (as response)
     let response = GetCommandIntentResponse.success(result: res)
         completion(response)
   }
